84082382477c87231d476d165272c57b639b3bf4
Akash Mozumdar <akashmozumdar@gmail.com>
2018-01-20 17:20:27 -0500

    use heroku

--- node.js
+++ node.js
@@ -3,4 +3,4 @@ const express = require("express");
 express()
   .get("/api/:test", (req, res) => res.status(200).json({ testing: req.params.test }))
   .use(express.static("build"))
-  .listen(3000);
+  .listen(process.env.PORT || 3000);
