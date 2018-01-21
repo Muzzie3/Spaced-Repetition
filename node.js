9f11273f2027a9eaf320de485229a77ca3d1ff1e
Akash Mozumdar <akashmozumdar@gmail.com>
2018-01-21 11:43:05 -0500

    get an api set up

--- node.js
+++ node.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mysql = require("mysql");
+const dbURL = require("./databaseURL") || process.env.CLEARDB_DATABASE_URL;
 
 express()
-  .get("/api/:test", (req, res) => res.status(200).json({ testing: req.params.test }))
+  .post("/api/createDeck/:user/:deck", (req, res) => {
+    const connection = mysql.createConnection(dbURL);
+    connection.query(
+      `insert into decks values (${req.params.user}, ${req.params.deck})`,
+      "",
+      err => (err ? res.status(500) : res.status(201)),
+    );
+  })
+  .get("/api/:data", (req, res) => res.status(200).json({ testing: req.params.data }))
   .use(express.static("build"))
   .listen(process.env.PORT || 3000);
