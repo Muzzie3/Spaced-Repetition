04c266c794c351cd4e2c50a945af6168dc3b9b68
Akash Mozumdar <akashmozumdar@gmail.com>
2018-01-27 17:37:52 -0500

    refactor

--- src/DeckDisplay.jsx
+++ src/DeckDisplay.jsx
@@ -4,17 +4,13 @@ const DeckDisplay = props => (
   <div>
     <button onClick={props.toTraining}>Train</button>
     <div />
-    <button
-      onClick={() =>
-        props.createCard(window.prompt("Front"), window.prompt("Back"))
-      }
-    >
+    <button onClick={() => props.createCard(window.prompt("Front"), window.prompt("Back"))}>
       Create
     </button>
     {props.cards.map(card => (
       <div key={card.id}>
-          {`Front: ${card.front} Back: ${card.back} Level: ${card.confidence} Time: ${new Date(card.time * 1000).toLocaleString()}`}
-          <button onClick={() => props.deleteCard(card.id)}>Delete</button>
+        {`Front: ${card.front} Back: ${card.back} Level: ${card.confidence} Time: ${new Date(card.time * 1000).toLocaleString()}`}
+        <button onClick={() => props.deleteCard(card.id)}>Delete</button>
       </div>
     ))}
   </div>
